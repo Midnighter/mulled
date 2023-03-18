f4bb18b91018fb34f6790d89c8816e2aa96e6871
Moritz E. Beber <midnighter@posteo.net>
2023-03-18 14:46:55 +0100

    refactor: adjust description slightly

--- src/components/Description.svelte
+++ src/components/Description.svelte
@@ -3,11 +3,11 @@
         href="https://github.com/BioContainers/multi-package-containers/blob/master/combinations/hash.tsv"
         >hash.tsv</a
     > file that tracks already built multi-package containers or fill in the information
-    below.
+    below. Pasting in content takes precedence over the filled in information.
 </p>
 
 <p>
-    Once you start filling in information, an image name will be generated and shown
+    Once you start adding information, an image name will be generated and shown
     above. That name will also be checked on <a href="https://quay.io">Quay.io</a> to
     see if such an image exists in the
     <a href="https://quay.io/organization/biocontainers">BioContainers organization</a>.
