d86fb6c7d0f08d3ad359962c959d3f12b920c69d
Moritz E. Beber <midnighter@posteo.net>
2023-03-18 23:18:53 +0100

    docs: explain check mark and cross

--- src/components/Description.svelte
+++ src/components/Description.svelte
@@ -11,4 +11,6 @@
     That name will also be checked on <a href="https://quay.io">Quay.io</a> to see if
     such an image exists in the
     <a href="https://quay.io/organization/biocontainers">BioContainers organization</a>.
+    If the image exists, you should see a &#10004; appear after a moment, otherwise
+    there will be a &#10060;.
 </p>
