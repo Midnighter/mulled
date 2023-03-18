f0d0b5501b57877353bcb93c8dba396d0472b707
Moritz E. Beber <midnighter@posteo.net>
2023-03-18 02:35:00 +0100

    chore: build static page with base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,8 @@ const config = {
         adapter: adapter(),
         alias: {
             $components: 'src/components'
-        }
+        },
+        paths: { base: process.env.NODE_ENV === 'production' ? '/mulled' : '' }
     }
 };
 
